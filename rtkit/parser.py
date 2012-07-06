506661a098da822789d0b47cd1a67db86c01973d
Andrea De Marco <24erre@gmail.com>
2012-07-06 12:18:30 +0200

    RTParser refactoring

---
+++ rtkit/parser.py
@@ -0,0 +1,101 @@
+from itertools import ifilterfalse
+import re
+from rtkit import comment
+
+
+class RTParser(object):
+    HEADER = re.compile(r'^RT/(?P<v>.+)\s+(?P<s>(?P<i>\d+).+)')
+    COMMENT = re.compile(r'^#\s+.+$')
+    SECTION = re.compile(r'^--', re.M | re.U)
+
+    @classmethod
+    def parse(cls, body, decoder):
+        r""" Return a list of RFC5322-like section
+        >>> decode = RTParser.decode
+        >>> body = '''
+        ...
+        ... # c1
+        ... spam: 1
+        ... ham: 2,
+        ...     3
+        ... eggs:'''
+        >>> RTParser.parse(body, decode)
+        [[('spam', '1'), ('ham', '2, 3'), ('eggs', '')]]
+        >>> RTParser.parse('# spam 1 does not exist.', decode)
+        Traceback (most recent call last):
+            ...
+        RTNotFoundError: spam 1 does not exist
+        >>> RTParser.parse('# Spam 1 created.', decode)
+        [[('id', 'spam/1')]]
+        >>> RTParser.parse('No matching results.', decode)
+        []
+        >>> decode = RTParser.decode_comment
+        >>> RTParser.parse('# spam: 1\n# ham: 2', decode)
+        [[('spam', '1'), ('ham', '2')]]
+        """
+        section = cls.build(body)
+        if len(section) == 1:
+            try:
+                comment.check(section[0])
+            except comment.RTNoMatch:
+                section = ''
+            except comment.RTCreated as e:
+                section = [['id: {0}'.format(e.id)]]
+        return [decoder(lines) for lines in section]
+
+    @classmethod
+    def decode(cls, lines):
+        """ Return a list of 2-tuples parsing 'k: v' and skipping comments
+        >>> RTParser.decode(['# c1: c2', 'spam: 1', 'ham: 2, 3', 'eggs:'])
+        [('spam', '1'), ('ham', '2, 3'), ('eggs', '')]
+        >>> RTParser.decode(['<!DOCTYPE HTML PUBLIC >', '<html><head>',])
+        []
+        """
+        try:
+            lines = ifilterfalse(cls.COMMENT.match, lines)
+            return [(k, v.strip(' ')) for k, v in [l.split(':', 1) for l in lines]]
+        except ValueError:
+            return []
+
+    @classmethod
+    def decode_comment(cls, lines):
+        """ Return a list of 2-tuples parsing '# k: v'
+        >>> RTParser.decode_comment(['# c1: c2', 'spam: 1', 'ham: 2, 3', 'eggs:'])
+        [('c1', 'c2')]
+        >>>
+        """
+        lines = filter(cls.COMMENT.match, lines)
+        return [(k.strip('# '), v.strip(' ')) for k, v in [l.split(':', 1) for l in lines]]
+
+    @classmethod
+    def build(cls, body):
+        """ Build logical lines from a RFC5322-like string
+        >>> body = '''RT/1.2.3 200 Ok
+        ...
+        ... # a
+        ...   b
+        ... spam: 1
+        ...
+        ... ham: 2,
+        ...     3
+        ... --
+        ... # c
+        ... spam: 4
+        ... ham:
+        ... --
+        ... a -- b
+        ... '''
+        >>> RTParser.build(body)
+        [['# a b', 'spam: 1', 'ham: 2, 3'], ['# c', 'spam: 4', 'ham:'], ['a -- b']]
+        """
+        def build_section(section):
+            logic_lines = []
+            for line in filter(None, section.splitlines()):
+                if cls.HEADER.match(line):
+                    continue
+                if line[0].isspace():
+                    logic_lines[-1] += ' ' + line.strip(' ')
+                else:
+                    logic_lines.append(line)
+            return logic_lines
+        return [build_section(b) for b in cls.SECTION.split(body)]
--- rtkit/resource.py
+++ rtkit/resource.py
@@ -1,10 +1,8 @@
-from itertools import ifilterfalse
 import logging
-import re
 import errors
 import forms
-import comment
 from urllib2 import Request, HTTPError
+from rtkit.parser import RTParser
 
 
 class RTResource(object):
@@ -40,10 +38,6 @@ class RTResource(object):
 
 
 class RTResponse(object):
-    HEADER = re.compile(r'^RT/(?P<v>.+)\s+(?P<s>(?P<i>\d+).+)')
-    COMMENT = re.compile(r'^#\s+.+$')
-    SECTION = re.compile(r'^--', re.M | re.U)
-
     def __init__(self, request, response):
         self.headers = response.headers
         self.body = response.read()
@@ -53,7 +47,7 @@ class RTResponse(object):
         self.logger.info(request.get_method())
         self.logger.info(request.get_full_url())
         self.logger.debug('HTTP_STATUS: {0}'.format(self.status))
-        r = self.HEADER.match(self.body)
+        r = RTParser.HEADER.match(self.body)
         if r:
             self.status = r.group('s')
             self.status_int = int(r.group('i'))
@@ -63,105 +57,13 @@ class RTResponse(object):
             self.status_int = 500
         self.logger.debug('%r' % self.body)
         try:
-            decoder = self._decode
+            decoder = RTParser.decode
             if self.status_int == 409:
-                decoder = self._decode_comment
-            self.parsed = self._parse(self.body, decoder)
+                decoder = RTParser.decode_comment
+            self.parsed = RTParser.parse(self.body, decoder)
         except errors.RTResourceError as e:
             self.parsed = []
             self.status_int = e.status_int
             self.status = '{0} {1}'.format(e.status_int, e.msg)
         self.logger.debug('RESOURCE_STATUS: {0}'.format(self.status))
         self.logger.info(self.parsed)
-
-    @classmethod
-    def _parse(cls, body, decoder):
-        r""" Return a list of RFC5322-like section
-        >>> decode = RTResponse._decode
-        >>> body = '''
-        ...
-        ... # c1
-        ... spam: 1
-        ... ham: 2,
-        ...     3
-        ... eggs:'''
-        >>> RTResponse._parse(body, decode)
-        [[('spam', '1'), ('ham', '2, 3'), ('eggs', '')]]
-        >>> RTResponse._parse('# spam 1 does not exist.', decode)
-        Traceback (most recent call last):
-            ...
-        RTNotFoundError: spam 1 does not exist
-        >>> RTResponse._parse('# Spam 1 created.', decode)
-        [[('id', 'spam/1')]]
-        >>> RTResponse._parse('No matching results.', decode)
-        []
-        >>> decode = RTResponse._decode_comment
-        >>> RTResponse._parse('# spam: 1\n# ham: 2', decode)
-        [[('spam', '1'), ('ham', '2')]]
-        """
-        section = cls._build(body)
-        if len(section) == 1:
-            try:
-                comment.check(section[0])
-            except comment.RTNoMatch:
-                section = ''
-            except comment.RTCreated as e:
-                section = [['id: {0}'.format(e.id)]]
-        return [decoder(lines) for lines in section]
-
-    @classmethod
-    def _decode(cls, lines):
-        """ Return a list of 2-tuples parsing 'k: v' and skipping comments
-        >>> RTResponse._decode(['# c1: c2', 'spam: 1', 'ham: 2, 3', 'eggs:'])
-        [('spam', '1'), ('ham', '2, 3'), ('eggs', '')]
-        >>> RTResponse._decode(['<!DOCTYPE HTML PUBLIC >', '<html><head>',])
-        []
-        """
-        try:
-            lines = ifilterfalse(cls.COMMENT.match, lines)
-            return [(k, v.strip(' ')) for k, v in [l.split(':', 1) for l in lines]]
-        except ValueError:
-            return []
-
-    @classmethod
-    def _decode_comment(cls, lines):
-        """ Return a list of 2-tuples parsing '# k: v'
-        >>> RTResponse._decode_comment(['# c1: c2', 'spam: 1', 'ham: 2, 3', 'eggs:'])
-        [('c1', 'c2')]
-        >>>
-        """
-        lines = filter(cls.COMMENT.match, lines)
-        return [(k.strip('# '), v.strip(' ')) for k, v in [l.split(':', 1) for l in lines]]
-
-    @classmethod
-    def _build(cls, body):
-        """ Build logical lines from a RFC5322-like string
-        >>> body = '''RT/1.2.3 200 Ok
-        ...
-        ... # a
-        ...   b
-        ... spam: 1
-        ...
-        ... ham: 2,
-        ...     3
-        ... --
-        ... # c
-        ... spam: 4
-        ... ham:
-        ... --
-        ... a -- b
-        ... '''
-        >>> RTResponse._build(body)
-        [['# a b', 'spam: 1', 'ham: 2, 3'], ['# c', 'spam: 4', 'ham:'], ['a -- b']]
-        """
-        def build_section(section):
-            logic_lines = []
-            for line in filter(None, section.splitlines()):
-                if cls.HEADER.match(line):
-                    continue
-                if line[0].isspace():
-                    logic_lines[-1] += ' ' + line.strip(' ')
-                else:
-                    logic_lines.append(line)
-            return logic_lines
-        return [build_section(b) for b in cls.SECTION.split(body)]
