ffb2229e6f18891dcbe8eb4dbd84ae087ee1bcde
Andrea De Marco <24erre@gmail.com>
2012-07-06 14:36:52 +0200

    tests skel

---
+++ rtkit/tests/mock.py
@@ -0,0 +1,24 @@
+import logging
+from flask.app import Flask
+from flask.globals import request
+from flask.helpers import make_response
+from rtkit.parser import RTParser
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+logging.root.addHandler(NullHandler())
+
+app = Flask(__name__)
+
+@app.route('/ticket/new', methods=['POST'])
+def create_tkt():
+    form = dict(RTParser.parse(request.form.get('content', ''), RTParser.decode)[0])
+    if form['Queue'] == 'ERR':
+        body = 'RT/3.8.10 200 Ok\n\n# Could not create ticket.\n# Could not create ticket. Queue not set\n\n'
+    elif form['Queue'] == 'NOPERM':
+        body = "RT/3.8.10 200 Ok\n\n# Could not create ticket.\n# No permission to create tickets in the queue '___Admin'\n\n"
+    else:
+        body = 'RT/3.8.10 200 Ok\n\n# Ticket 1 created.\n\n'
+    response = make_response(body, 200)
+    return response
---
+++ rtkit/tests/ticket.py
@@ -0,0 +1,56 @@
+from rtkit.tests.common import TestCaseWithFlask
+from rtkit.tests.mock import app
+from rtkit.resource import RTResource
+from rtkit.authenticators import AbstractAuthenticator
+
+
+class TestCaseRT(TestCaseWithFlask):
+    application = app
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCaseRT, cls).setUpClass()
+        cls.resource = RTResource('http://localhost:5000/', None, None, AbstractAuthenticator)
+
+    def test_create_tkt(self):
+        message = 'My useless\ntext on\nthree lines.'
+        content = {
+                'content': {
+                            'Queue': 1,
+                            'Subject' : 'New Ticket',
+                            'Text' : message.replace('\n', '\n '),
+                        }
+        }
+        response = self.resource.post(path='ticket/new', payload=content,)
+        self.assertEqual(response.parsed, [[('id', 'ticket/1')]])
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.status, '200 Ok')
+
+    def test_create_tkt_noqueue(self):
+        message = 'My useless\ntext on\nthree lines.'
+        content = {
+                'content': {
+                            'Queue': 'ERR',
+                            'Subject' : 'New Ticket',
+                            'Text' : message.replace('\n', '\n '),
+                        }
+        }
+        response = self.resource.post(path='ticket/new', payload=content,)
+        self.assertEqual(response.parsed, [])
+        self.assertEqual(response.status_int, 400)
+        self.assertEqual(response.status, '400 Could not create ticket. Queue not set')
+
+    def test_create_tkt_noperm(self):
+        message = 'My useless\ntext on\nthree lines.'
+        content = {
+                'content': {
+                            'Queue': 'NOPERM',
+                            'Subject' : 'New Ticket',
+                            'Text' : message.replace('\n', '\n '),
+                        }
+        }
+        response = self.resource.post(path='ticket/new', payload=content,)
+        self.assertEqual(response.parsed, [])
+        self.assertEqual(response.status_int, 400)
+        self.assertEqual(response.status, "400 No permission to create tickets in the queue '___Admin'")
+
