be7927b36e1b6d55f4e1fd8530253157f75b6939
Andrea De Marco <24erre@gmail.com>
2012-07-06 15:54:01 +0200

    README examples in test

--- rtkit/tests/ticket.py
+++ rtkit/tests/ticket.py
@@ -4,12 +4,12 @@ from rtkit.resource import RTResource
 from rtkit.authenticators import AbstractAuthenticator
 
 
-class TestCaseRT(TestCaseWithFlask):
+class TestCaseTicket(TestCaseWithFlask):
     application = app
 
     @classmethod
     def setUpClass(cls):
-        super(TestCaseRT, cls).setUpClass()
+        super(TestCaseTicket, cls).setUpClass()
         cls.resource = RTResource('http://localhost:5000/', None, None, AbstractAuthenticator)
 
     def test_create_tkt(self):
@@ -30,7 +30,7 @@ class TestCaseRT(TestCaseWithFlask):
         message = 'My useless\ntext on\nthree lines.'
         content = {
             'content': {
-                'Queue': 'ERR',
+                'Queue': 2,
                 'Subject': 'New Ticket',
                 'Text': message.replace('\n', '\n '),
             }
@@ -44,7 +44,7 @@ class TestCaseRT(TestCaseWithFlask):
         message = 'My useless\ntext on\nthree lines.'
         content = {
             'content': {
-                'Queue': 'NOPERM',
+                'Queue': 3,
                 'Subject': 'New Ticket',
                 'Text': message.replace('\n', '\n '),
             }
@@ -53,3 +53,45 @@ class TestCaseRT(TestCaseWithFlask):
         self.assertEqual(response.parsed, [])
         self.assertEqual(response.status_int, 400)
         self.assertEqual(response.status, "400 No permission to create tickets in the queue '___Admin'")
+
+    def test_read_tkt(self):
+        response = self.resource.get(path='ticket/1')
+        self.assertEqual(response.parsed, [[
+            ('id', 'ticket/1'),
+            ('Queue', 'General'),
+            ('Owner', 'Nobody'),
+            ('Creator', 'pyrtkit'),
+            ('Subject', 'pyrt-create4'),
+            ('Status', 'open'),
+            ('Priority', '5'),
+            ('InitialPriority', '0'),
+            ('FinalPriority', '0'),
+            ('Requestors', ''),
+            ('Cc', ''),
+            ('AdminCc', ''),
+            ('Created', 'Sun Jul 03 10:48:57 2011'),
+            ('Starts', 'Not set'),
+            ('Started', 'Not set'),
+            ('Due', 'Not set'),
+            ('Resolved', 'Not set'),
+            ('Told', 'Wed Jul 06 12:58:00 2011'),
+            ('LastUpdated', 'Thu Jul 07 14:42:32 2011'),
+            ('TimeEstimated', '0'),
+            ('TimeWorked', '25 minutes'),
+            ('TimeLeft', '0'),
+        ]])
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.status, '200 Ok')
+
+    def test_read_tkt_notfound(self):
+        response = self.resource.get(path='ticket/2')
+        self.assertEqual(response.parsed, [])
+        self.assertEqual(response.status_int, 404)
+        self.assertEqual(response.status, '404 Ticket 2 does not exist')
+
+    def test_update_tkt_syntax(self):
+        content = {'content': {'Queue': 3, }}
+        response = self.resource.post(path='ticket/1', payload=content,)
+        self.assertEqual(response.parsed, [[('queue', 'You may not create requests in that queue.')]])
+        self.assertEqual(response.status_int, 409)
+        self.assertEqual(response.status, '409 Syntax Error')
