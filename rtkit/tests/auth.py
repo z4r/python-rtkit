7960ae7ed81c530919706d125b0839b87e892f1e
ademarco <24erre@gmail.com>
2012-12-10 15:24:42 +0100

    0.3 [test restyle]

---
+++ rtkit/tests/auth.py
@@ -0,0 +1,62 @@
+import base64
+import unittest
+from httpretty import httprettified, HTTPretty
+from rtkit.resource import RTResource
+from rtkit.authenticators import BasicAuthenticator, CookieAuthenticator
+
+
+class BasicAuthTestCase(unittest.TestCase):
+    def setUp(self):
+        self.resource = RTResource('http://rtkit.test/', 'USER', 'PASS', BasicAuthenticator)
+
+    @httprettified
+    def test_auth(self):
+        HTTPretty.register_uri(
+            HTTPretty.GET,
+            'http://rtkit.test/ticket/1',
+            responses=[
+                HTTPretty.Response(body='', status=401, www_authenticate='Basic realm="rtkit.test"'),
+                HTTPretty.Response(body='RT/3.8.10 200 Ok\n\n# Ticket 1 does not exist.\n\n\n', status=200),
+            ]
+        )
+        self.resource.get(
+            path='ticket/1',
+            headers={'User-Agent': 'rtkit-ua', }
+        )
+        self.assertEqual(
+            HTTPretty.last_request.headers['authorization'],
+            'Basic ' + base64.b64encode('USER:PASS'),
+        )
+
+
+class CookieAuthTestCase(unittest.TestCase):
+    def setUp(self):
+        self.resource = RTResource('http://rtkit.test/', 'USER', 'PASS', CookieAuthenticator)
+
+    @httprettified
+    def test_auth(self):
+        HTTPretty.register_uri(
+            HTTPretty.POST,
+            'http://rtkit.test/',
+            body='RT/4.0.5 200 Ok',
+            status=200,
+            set_cookie='RT_SID_example.com.80=2ec1d46cb1c05fff0fce34ed268e7d26; path=/; HttpOnly',
+            transfer_encoding='chunked'
+        )
+        HTTPretty.register_uri(
+            HTTPretty.GET,
+            'http://rtkit.test/ticket/1',
+            body='RT/3.8.10 200 Ok\n\n# Ticket 1 does not exist.\n\n\n',
+            status=200
+        )
+        self.resource.get(
+            path='ticket/1',
+            headers={'User-Agent': 'rtkit-ua', }
+        )
+        self.assertEqual(len(HTTPretty.latest_requests), 2)
+        self.assertEqual(HTTPretty.latest_requests[0].path, '/')
+        self.assertEqual(HTTPretty.latest_requests[0].method, 'POST')
+        self.assertEqual(HTTPretty.latest_requests[0].body, 'user=USER&pass=PASS')
+        self.assertEqual(HTTPretty.latest_requests[1].path, '/ticket/1')
+        self.assertEqual(HTTPretty.latest_requests[1].method, 'GET')
+        self.assertEqual(HTTPretty.latest_requests[1].body, '')
--- rtkit/tests/tkt.py
+++ rtkit/tests/tkt.py
@@ -4,7 +4,7 @@ from httpretty import httprettified, HTTPretty
 from rtkit.resource import RTResource
 from rtkit.authenticators import AbstractAuthenticator
 
-Expected = namedtuple('Expected', 'req_body parsed status_int status')
+Expected = namedtuple('Expected', 'req_body req_headers parsed status_int status')
 
 
 class TktTestCase(unittest.TestCase):
@@ -18,28 +18,49 @@ class TktTestCase(unittest.TestCase):
             }
         }
         self.req_body = 'content=Queue: 1\nText: My useless\n text on\n three lines.\nSubject: New Ticket'
+        self.req_headers_get = {
+            'connection': 'close',
+            'user-agent': 'rtkit-ua',
+            'host': 'rtkit.test',
+            'accept': 'text/plain',
+            'accept-encoding': 'identity',
+        }
+        self.req_headers_post = self.req_headers_get.copy()
+        self.req_headers_post.update({
+            'content-length': '76',
+            'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+        })
 
     @httprettified
     def assertPost(self, body, expected, content=None):
         HTTPretty.register_uri(HTTPretty.POST, 'http://rtkit.test/ticket/new', body=body)
-        response = self.resource.post(path='ticket/new', payload=content or self.content)
+        response = self.resource.post(
+            path='ticket/new',
+            payload=content or self.content,
+            headers={'User-Agent': 'rtkit-ua', }
+        )
         self.assertEqual(response.parsed, expected.parsed)
         self.assertEqual(response.status_int, expected.status_int)
         self.assertEqual(response.status, expected.status)
         self.assertEqual(HTTPretty.last_request.method, HTTPretty.POST)
         self.assertEqual(HTTPretty.last_request.path, '/ticket/new')
         self.assertEqual(HTTPretty.last_request.body, expected.req_body)
+        self.assertEqual(dict(HTTPretty.last_request.headers), expected.req_headers)
 
     @httprettified
     def assertGet(self, body, expected):
         HTTPretty.register_uri(HTTPretty.GET, 'http://rtkit.test/ticket/1', body=body)
-        response = self.resource.get(path='ticket/1')
+        response = self.resource.get(
+            path='ticket/1',
+            headers={'User-Agent': 'rtkit-ua', }
+        )
         self.assertEqual(response.parsed, expected.parsed)
         self.assertEqual(response.status_int, expected.status_int)
         self.assertEqual(response.status, expected.status)
         self.assertEqual(HTTPretty.last_request.method, HTTPretty.GET)
         self.assertEqual(HTTPretty.last_request.path, '/ticket/1')
         self.assertEqual(HTTPretty.last_request.body, expected.req_body)
+        self.assertEqual(dict(HTTPretty.last_request.headers), expected.req_headers)
 
     def test_create_tkt(self):
         expected = Expected(
@@ -47,6 +68,7 @@ class TktTestCase(unittest.TestCase):
             status_int=200,
             status='200 Ok',
             req_body=self.req_body,
+            req_headers=self.req_headers_post,
         )
         self.assertPost(
             body='RT/3.8.10 200 Ok\n\n# Ticket 1 created.\n\n',
@@ -59,6 +81,7 @@ class TktTestCase(unittest.TestCase):
             status_int=400,
             status='400 Could not create ticket. Queue not set',
             req_body=self.req_body,
+            req_headers=self.req_headers_post,
         )
         self.assertPost(
             body='RT/3.8.10 200 Ok\n\n# Could not create ticket.\n# Could not create ticket. Queue not set\n\n',
@@ -71,6 +94,7 @@ class TktTestCase(unittest.TestCase):
             status_int=400,
             status='400 No permission to create tickets in the queue \'___Admin\'',
             req_body=self.req_body,
+            req_headers=self.req_headers_post,
         )
         self.assertPost(
             body='RT/3.8.10 200 Ok\n\n# Could not create ticket.\n# No permission to create tickets in the queue \'___Admin\'\n\n',
@@ -106,6 +130,7 @@ class TktTestCase(unittest.TestCase):
             status_int=200,
             status='200 Ok',
             req_body='',
+            req_headers=self.req_headers_get,
         )
         self.assertGet(
             body='''RT/3.8.10 200 Ok
@@ -144,6 +169,7 @@ TimeLeft: 0
             status_int=404,
             status='404 Ticket 1 does not exist',
             req_body='',
+            req_headers=self.req_headers_get,
         )
         self.assertGet(
             body='RT/3.8.10 200 Ok\n\n# Ticket 1 does not exist.\n\n\n',
@@ -156,6 +182,7 @@ TimeLeft: 0
             status_int=401,
             status='401 Credentials required',
             req_body='',
+            req_headers=self.req_headers_get,
         )
         self.assertGet(
             body='RT/3.8.10 401 Credentials required\n',
@@ -163,11 +190,13 @@ TimeLeft: 0
         )
 
     def test_update_tkt_syntax_error(self):
+        self.req_headers_post.update({'content-length': '16'})
         expected = Expected(
             parsed=[[('queue', 'You may not create requests in that queue.')]],
             status_int=409,
             status='409 Syntax Error',
             req_body='content=Queue: 3',
+            req_headers=self.req_headers_post,
         )
         self.assertPost(
             body='RT/3.8.10 409 Syntax Error\n\n# queue: You may not create requests in that queue.\n\n',
@@ -176,11 +205,16 @@ TimeLeft: 0
         )
 
     def test_tkt_comment_with_attach(self):
+        self.req_headers_post.update({
+            'content-length': '760',
+            'content-type': 'multipart/form-data; boundary=xXXxXXyYYzzz',
+        })
         expected = Expected(
             parsed=[[]],
             status_int=200,
             status='200 Ok',
             req_body='--xXXxXXyYYzzz\r\nContent-Disposition: form-data; name="content"\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 77\r\n\r\nAction: comment\nText: Comment with attach\nAttachment: x1.txt, x2.txt, 1x1.gif\r\n--xXXxXXyYYzzz\r\nContent-Disposition: form-data; name="attachment_2"; filename="rtkit/tests/attach/x2.txt"\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello World!\n2\n\r\n--xXXxXXyYYzzz\r\nContent-Disposition: form-data; name="attachment_1"; filename="rtkit/tests/attach/x1.txt"\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello World!\n1\n\r\n--xXXxXXyYYzzz\r\nContent-Disposition: form-data; name="attachment_3"; filename="rtkit/tests/attach/1x1.gif"\r\nContent-Type: image/gif\r\nContent-Length: 35\r\n\r\nGIF87a\x01\x00\x01\x00\x80\x00\x00\xcc\xcc\xcc\x96\x96\x96,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;\r\n--xXXxXXyYYzzz--\r\n',
+            req_headers=self.req_headers_post,
         )
         self.assertPost(
             body='RT/3.8.10 200 Ok\n\n# Message recorded\n\n',
