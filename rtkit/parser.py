9cb1f9cfbc136b4c45349709fa564e3f1a9b8a54
Иван Багаев <gmiramir@gmail.com>
2013-01-14 13:52:16 +0600

    Update rtkit/parser.py
    correct except syntax for python3

--- rtkit/parser.py
+++ rtkit/parser.py
@@ -54,7 +54,7 @@ class RTParser(object):
         try:
             lines = ifilterfalse(cls.COMMENT.match, lines)
             return [(k, v.strip(' ')) for k, v in [l.split(':', 1) for l in lines]]
-        except ValueError, IndexError:
+        except (ValueError, IndexError):
             return []
 
     @classmethod
