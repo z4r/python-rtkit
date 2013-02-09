eefbda8cd89790f96419d0eb4a45e9e6bc3f7ac2
Andrea de Marco <24erre@gmail.com>
2013-02-09 13:10:17 +0100

    bump version

--- rtkit/__init__.py
+++ rtkit/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Andrea De Marco <24erre@gmail.com>'
-__version__ = '0.3.1'
+__version__ = '0.4.1'
 __classifiers__ = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
